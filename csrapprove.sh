4f0fa5c52dee7541fe35232dd2fe740dd9b9132f
natalie-elizabeth <natalie.elizabeth@andela.com>
2020-09-03 15:42:14 +0300

    updates csrapprove

--- csrapprove.sh
+++ csrapprove.sh
@@ -17,4 +17,10 @@ EOF
 kubectl certificate approve nginx.argo
 
 # get cert file
-kubectl get csr nginx.argo -n tls -o jsonpath='{.status.certificate}' | base64 --decode > server.crt
+kubectl get csr nginx.argo -n argo -o jsonpath='{.status.certificate}' | base64 --decode > server.crt
+
+echo ("we get here")
+# create secret and cm
+kubectl create secret tls nginxsecret -n argo --key ca-key.pem --cert server.crt
+kubectl get secrets -n argo
+kubectl create cm nginxconfigmap --from-file=default.conf -n argo
